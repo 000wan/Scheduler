5d893f725e78b7a2b740e6d58c68b97665064a20
KYW <coder1023@naver.com>
2022-08-09 21:52:54 +0900

    .

---
+++ main.py
@@ -0,0 +1,9 @@
+# =========================
+# 'Scheduler' by 000wan
+# =========================
+# Description
+#   : University Schedule Generator with probabilities
+#
+# 2022/08/09 : Development with python starts
+#
+
